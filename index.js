a59b89a0ccd3f5e812073a7a78504568344c0361
jcooper-6 <jcooper6@ufl.edu>
2026-03-04 11:04:57 +0900

    Added basic function declarations

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "8e2d81f77bf781eee278d91811303875";
+
+weatherForm.addEventListener("submit", event => {
+
+});
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function displayError(message){
+
+}
