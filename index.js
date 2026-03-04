bba38adfde6de4efa09d829913057473022d938c
jcooper-6 <jcooper6@ufl.edu>
2026-03-04 12:49:42 +0900

    Implemented displayWeatherInfo to display the current temp and humidity when a city name is entered

--- index.js
+++ index.js
@@ -27,10 +27,9 @@ weatherForm.addEventListener("submit", async event => {
 
 async function getWeatherData(city){
 
+    //Edit this api call here to get different data
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     const response = await fetch(apiUrl);
-
-    console.log(response);
 
     if (!response.ok) {
         throw new Error("Could not fetch weather data");
@@ -39,7 +38,25 @@ async function getWeatherData(city){
 }
 
 function displayWeatherInfo(data){
+    
+    const {name: city, main: {temp, humidity}} = data;
+    
+    card.textContent = "";
+    card.style.display = "flex";
 
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${(temp).toFixed(1)}°F`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+
+    // Add css style classes here
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
 }
 
 function displayError(message){
